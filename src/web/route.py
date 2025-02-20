078fe870b78b03574ff9d69b8634ae805ebef3b1
Mariano <marianortega593@gmail.com>
2025-02-20 20:36:43 -0300

    algunos templates, controllers y crud

--- src/web/route.py
+++ src/web/route.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template
 from config.config import env
 from config.database import db
-
+from src.web.controllers.turno import bp as turno_bp
 def register(app):
     @app.route("/")
     def home():
         return render_template("index.html")
 
+    app.register_blueprint(turno_bp)
 
 
